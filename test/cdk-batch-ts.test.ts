4592d6842d3d5502632ebc3e1ce169bf117b43c9
Tim Cutts <tim.cutts@astrazeneca.com>
2024-03-08 16:09:28 +0000

    Initial commit

---
+++ test/cdk-batch-ts.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkBatchTs from '../lib/cdk-batch-ts-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-batch-ts-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkBatchTs.CdkBatchTsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
